409de604bc08241fcd35e64c9654a8d5925d2f29
pqhieu02 <hieunau405@gmail.com>
2021-05-24 15:37:22 +0300

    comments modified

--- wlan/analyse.c
+++ wlan/analyse.c
@@ -18,7 +18,7 @@ void analyse(FILE* fi) {
 			strcpy(field, fieldIdentifier(c));
 			//printf("%s\n", field);
 
-			// Get SSID infor
+			// Get SSID 
 			if (strcmp(field, "SSID") == 0) {
 				//printf("%s : %s\n", field, getValue(c));
 
@@ -33,7 +33,7 @@ void analyse(FILE* fi) {
 				//printf("%s\n", ssid_list[ssid_index].ssid);
 			}
 
-			//Get BSSID infor
+			//Get BSSID 
 			if (strcmp(field, "BSSID") == 0) {
 				//printf("%s : %s\n", field, getValue(c));
 				ap_index++;
@@ -41,7 +41,7 @@ void analyse(FILE* fi) {
 				strcpy(ssid_list[ssid_index].ap_list[ap_index].mac_address, getValue(c));
 			}
 
-			//get Channel infor
+			//get Channel 
 			if (strcmp(field, "Channel") == 0) {
 				//printf("%s : %s\n", field, getValue(c));
 				ssid_list[ssid_index].ap_list[ap_index].channel = atoi(getValue(c));
